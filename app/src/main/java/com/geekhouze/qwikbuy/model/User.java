ef18f266de1db76668183c2a82f52f48d758ad1a
geoff <geoff.seloana@gmail.com>
2018-08-16 20:26:34 +0200

    Model

---
+++ app/src/main/java/com/geekhouze/qwikbuy/model/User.java
@@ -0,0 +1,121 @@
+
+package com.geekhouze.qwikbuy.model;
+
+import com.google.gson.annotations.Expose;
+
+
+@SuppressWarnings("unused")
+public class User {
+
+    @Expose
+    private String address;
+    @Expose
+    private String createdAt;
+    @Expose
+    private String email;
+    @Expose
+    private Long id;
+    @Expose
+    private String name;
+    @Expose
+    private String password;
+    @Expose
+    private String phone;
+    @Expose
+    private String surname;
+    @Expose
+    private Object token;
+    @Expose
+    private String updatedAt;
+    @Expose
+    private String username;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Object getToken() {
+        return token;
+    }
+
+    public void setToken(Object token) {
+        this.token = token;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
